45397f782ecb700777ab0c39a4d780579fdc86b2
LukasAVB <lukasavb@gmail.com>
2024-11-16 18:32:25 +0700

    test: config update

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.optimization = {
+      ...config.optimization,
+      minimize: true, // Ensure minification is enabled
+    };
+    return config;
+  },
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
