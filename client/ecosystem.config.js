f74ecc100dc73425bde729f2a20ca0e80726913f
Luis Munoz <lmunoz@cryptorepair.com>
2024-11-16 21:32:28 +0700

    fix: will come back to jenkins issue

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       exec_mode: 'fork', // Use 'cluster' for multiple instances
       instances: 1, // Set to 'max' for all CPU cores
       env: {
-        NODE_ENV: 'production',
+        NEXT_PUBLIC_PAGE_TITLE: process.env.NEXT_PUBLIC_PAGE_TITLE,
         NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
         PRIVY_APP_SECRET: process.env.PRIVY_APP_SECRET,
         PORT: process.env.PORT || 3000
