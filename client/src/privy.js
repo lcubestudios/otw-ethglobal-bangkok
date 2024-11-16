d598e5b94e7daf5f9def72720116fd2994608f2f
LukasAVB <lukasavb@gmail.com>
2024-11-16 14:50:55 +0700

    feat: basic landing page w/ privy disabled

--- client/src/privy.js
+++ client/src/privy.js
@@ -1,15 +1,15 @@
-import { PrivyClient } from "@privy-io/privy-browser";
+// import { PrivyClient } from "@privy-io/privy-browser";
 
-const privy = new PrivyClient({
-  appId: process.env.REACT_APP_PRIVY_APP_ID,
-});
+// const privy = new PrivyClient({
+//   appId: process.env.REACT_APP_PRIVY_APP_ID,
+// });
 
-export const loginUser = async () => {
-  try {
-    const user = await privy.login();
-    console.log("User logged in:", user);
-    return user;
-  } catch (error) {
-    console.error("Login failed:", error);
-  }
-};
+// export const loginUser = async () => {
+//   try {
+//     const user = await privy.login();
+//     console.log("User logged in:", user);
+//     return user;
+//   } catch (error) {
+//     console.error("Login failed:", error);
+//   }
+// };
