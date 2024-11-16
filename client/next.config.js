79bcdac8b894f9c078f4a2b029cf95ec93081eee
LukasAVB <lukasavb@gmail.com>
2024-11-16 18:45:19 +0700

    refactor: remove unused config

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,4 @@
 const nextConfig = {
-  webpack: (config) => {
-    config.optimization = {
-      ...config.optimization,
-      minimize: true, // Ensure minification is enabled
-    };
-    return config;
-  },
   reactStrictMode: true
 };
 
